668c72094625a9adb428936340f84d59e827ef84
vern-gora <103502360+vern-gora@users.noreply.github.com>
2023-06-06 01:29:36 +0300

    hw7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+const markup = galleryItems.map((item) => {
+  const galleryItemEl = document.createElement("li");
+  galleryItemEl.classList.add("gallery__item");
+  const galleryLinkEl = document.createElement("a");
+  galleryLinkEl.classList.add("gallery__link");
+  galleryItemEl.append(galleryLinkEl);
+  galleryLinkEl.href = item.original;
+  const galleryImgEl = document.createElement("img");
+  galleryImgEl.classList.add("gallery__image");
+  galleryLinkEl.append(galleryImgEl);
+  galleryImgEl.src = item.preview;
+  galleryImgEl.setAttribute("data-source", item.original);
+  galleryImgEl.alt = item.description;
+  return galleryItemEl;
+});
+galleryEl.append(...markup);
+
+galleryEl.addEventListener("click", selectImg);
+function selectImg(event) {
+  event.preventDefault();
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+  instance.show();
+}
+galleryEl.addEventListener("keyup", (event) => {
+  if (event.key === "Escape") {
+    instance.close();
+  }
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryListEl = document.querySelector(".gallery");
+
+const galleryItemsEl = galleryItems
+  .map(({ preview, original, description }) => {
+    let galleryList = `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a></li>`;
+    return galleryList;
+  })
+  .join("");
+
+galleryListEl.insertAdjacentHTML("beforeend", galleryItemsEl);
+
+const gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
