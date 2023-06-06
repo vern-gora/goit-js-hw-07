ae2247674b0d05fd6438daf4cf060466cb76dbce
vern-gora <103502360+vern-gora@users.noreply.github.com>
2023-06-06 19:00:57 +0300

    fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,33 +4,44 @@ import { galleryItems } from "./gallery-items.js";
 console.log(galleryItems);
 
 const galleryEl = document.querySelector(".gallery");
-const markup = galleryItems.map((item) => {
-  const galleryItemEl = document.createElement("li");
-  galleryItemEl.classList.add("gallery__item");
-  const galleryLinkEl = document.createElement("a");
-  galleryLinkEl.classList.add("gallery__link");
-  galleryItemEl.append(galleryLinkEl);
-  galleryLinkEl.href = item.original;
-  const galleryImgEl = document.createElement("img");
-  galleryImgEl.classList.add("gallery__image");
-  galleryLinkEl.append(galleryImgEl);
-  galleryImgEl.src = item.preview;
-  galleryImgEl.setAttribute("data-source", item.original);
-  galleryImgEl.alt = item.description;
-  return galleryItemEl;
-});
-galleryEl.append(...markup);
+const markup = galleryItems
+  .map((item) => {
+    return `<li class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                <img
+                class="gallery__image"
+                src="${item.preview}"
+                data-source="${item.original}"
+                alt="${item.description}"
+                />
+            </a>
+        </li>`;
+  })
+  .join("");
+galleryEl.insertAdjacentHTML("beforeend", markup);
 
 galleryEl.addEventListener("click", selectImg);
 function selectImg(event) {
   event.preventDefault();
-  const instance = basicLightbox.create(`
+  if (event.target.tagName === "IMG") {
+    const instance = basicLightbox.create(
+      `
     <img src="${event.target.dataset.source}" width="800" height="600">
-`);
-  instance.show();
-}
-galleryEl.addEventListener("keyup", (event) => {
-  if (event.key === "Escape") {
-    instance.close();
+`,
+      {
+        onShow: () => {
+          document.addEventListener("keyup", closeEsc);
+        },
+        onClose: () => {
+          document.removeEventListener("keyup", closeEsc);
+        },
+      }
+    );
+    instance.show();
+    function closeEsc(event) {
+      if (event.key === "Escape") {
+        instance.close();
+      }
+    }
   }
-});
+}
